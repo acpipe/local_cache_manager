cb2bbf84354495999aeb15dbdad88bf7707e95cf
huming1 <huming1@xiaomi.com>
2018-01-03 21:11:26 +0800

    add lombok checkstyle log4j2

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,15 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
-    
     @RequestMapping("/")
     public String index() {
+        log.info("start");
         return "Greetings from Spring Boot!";
     }
-    
 }
