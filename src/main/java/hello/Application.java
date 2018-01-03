0074b9c9d427f5c01987ffea76b75f2f63f1cf21
huming1 <huming1@xiaomi.com>
2018-01-03 21:11:26 +0800

    add lombok checkstyle log4j2

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,14 +1,17 @@
 package hello;
 
-import java.util.Arrays;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
+
+@Slf4j
 @SpringBootApplication
+@SuppressWarnings("uncommentedmain")
 public class Application {
 
     public static void main(String[] args) {
@@ -18,16 +21,12 @@ public class Application {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
+            log.info("Let's inspect the beans provided by Spring Boot:");
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
-                System.out.println(beanName);
+                log.info(beanName);
             }
-
         };
     }
-
 }
